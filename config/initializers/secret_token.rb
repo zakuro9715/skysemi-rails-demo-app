83557cf1d499cb6bae1e668092c2adb5115de9f4
YuZakuro <c1239@s.akashi.ac.jp>
2014-05-31 00:14:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkysemiDemoApp::Application.config.secret_key_base = 'e64749248f6326e6855af5cb25c3577c110ab3013002536e7bba2a432804384701c22834443dd45c6f4210ad853d7e1f291498c0fb66cd01dd72931556b0f2eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkysemiDemoApp::Application.config.session_store :cookie_store, key: '_skysemi_demo_app_session'
